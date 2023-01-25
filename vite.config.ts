921d9923386360003da5ff95e81117a82c87fd98
Shannon Sumner <108552773+shannonsumner@users.noreply.github.com>
2023-01-25 17:39:47 -0500

    Added export of types

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,37 @@
-import {defineConfig} from 'vite'
-import vue from '@vitejs/plugin-vue'
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import vue from '@vitejs/plugin-vue';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/main.ts'),
-            name: 'MyLib',
-            fileName: (format) => `aem-vue-editable-components.${format}.js`,
-        },
-        rollupOptions: {
-            external: ['vue'],
-            output: {
-                // Provide global variables to use in the UMD build
-                // Add external deps here
-                globals: {
-                    vue: 'Vue',
-                },
-            },
-        },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      name: 'aemReactEditableComponents',
+      fileName: (format) => `aem-vue-editable-components.${format}.js`,
     },
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './src'),
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // Add external deps here
+        globals: {
+          vue: 'Vue',
         },
+      },
     },
-    plugins: [vue()],
-})
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [
+    vue(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
+});
